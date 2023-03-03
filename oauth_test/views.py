245e76f71ee93db1d6e73c025225deae9a184f25
nayan2070 <nayangupta2070@gmail.com>
2023-03-04 01:59:53 +0530

    intial commit

---
+++ oauth_test/views.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import User, Group
+from django.shortcuts import render
+
+
+# Create your views here.
+from django.db import models
+from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope, TokenHasScope
+from rest_framework import generics, permissions
+# from rest_framework import serializers
+
+from oauth_test.serializer import UserSerializer, GroupSerializer
+
+
+class UserList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetails(generics.RetrieveAPIView):
+    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class GroupList(generics.ListAPIView):
+    permission_classes = [permissions.IsAuthenticated, TokenHasScope]
+    required_scopes = ['groups']
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
